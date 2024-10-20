e80fcc118f6c275f852adf0c0efdb62e518fb684
Benjy Marks <benjy.marks@sydney.edu.au>
2024-10-20 12:02:20 +1100

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js', // Entry point of your app
+    output: {
+        filename: 'bundle.js', // Output bundle name
+        path: path.resolve(__dirname, 'dist'), // Output directory
+        clean: true, // Clean the dist folder before each build
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'], // Transpile modern JS
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'], // Handle CSS imports
+            },
+        ],
+    },
+    devServer: {
+        static: './dist', // Serve from the dist folder
+        open: true, // Automatically open the browser
+        hot: true, // Enable hot module replacement
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/html/index.html', // Specify HTML template
+        }),
+    ],
+    mode: 'development',
+};
